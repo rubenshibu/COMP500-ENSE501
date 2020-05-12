51a3d058dab95d6a2a5598deb4cabf621703349a
rubenshibu <rubenshibu@gmail.com>
2020-05-12 15:41:06 +0530

    submitting

---
+++ more_pseudo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void){
+    int x,y,z;
+    scanf("%d%d%d",x,y,z);
+    int a = x*x;
+    int b = y*y;
+    int c = z*z;
+    printf("result %d",a+b+c);
+}
---
+++ pseudo_vector.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+    int x,y,z;
+    printf("x?");
+    scanf("%d",&x);
+    printf("y?");
+    scanf("%d",&y);    
+    printf("z?");
+    scanf("%d",&z);
+}
